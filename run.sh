82c67bb80515465abe9219dea9c3d3984ffdc9c2
Parker Smith <parker@parker.ws>
2021-03-13 10:51:46 -0600

    2.2.11 freebsd

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 find /root/.polyglot/nodeservers/ -name 'requirements.txt' -exec pip3 install -r {} \;
-/opt/udi-polyglotv2/polyglot-v2-linux-x64
+/opt/udi-polyglotv2/polyglot-v2-freebsd-x64
